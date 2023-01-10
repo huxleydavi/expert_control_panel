5c5a2c5f5ae1e1196a229c02a4767b122521e21b
Huxley <huxdavi@gmail.com>
2023-01-10 01:58:38 -0300

    Add files via upload

---
+++ vendor/mpdf/ttfontdata/latto.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='Lato-Regular';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 717.0,
+  'XHeight' => 507.0,
+  'FontBBox' => '[-547 -269 1343 1079]',
+  'Flags' => 4,
+  'Ascent' => 987.0,
+  'Descent' => -213.0,
+  'Leading' => 0.0,
+  'ItalicAngle' => 0.0,
+  'StemV' => 87.0,
+  'MissingWidth' => 532.0,
+);
+$unitsPerEm=2000;
+$up=-233;
+$ut=45;
+$strp=275;
+$strs=45;
+$ttffile='C:/xampp/htdocs/ead/app/vendor/mpdf/mpdf/ttfonts/Lato.ttf';
+$TTCfontID='0';
+$originalsize=657212;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='latto';
+$panose=' 0 0 2 f 5 2 2 2 4 3 2 3';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 805, -195, 200
+// usWinAscent/usWinDescent = 987, -213
+// hhea Ascent/Descent/LineGap = 987, -213, 0
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
